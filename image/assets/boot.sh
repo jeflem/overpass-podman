0d38548deba78e2e3582dacd021573747ce95ff4
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-06-11 11:12:32 +0200

    Automatically download and install diffs

--- image/assets/boot.sh
+++ image/assets/boot.sh
@@ -8,9 +8,6 @@ chmod -R a+rw /opt/osm_db
 cd /home/osm_user
 if ! test -f /opt/osm_db/nodes.bin; then
     echo "No db found. Populating db..."
-    runuser -l  osm_user -c 'wget --no-check-certificate https://download.geofabrik.de/europe/luxembourg-latest.osm.bz2'
-    runuser -l  osm_user -c 'osm-3s/bin/init_osm3s.sh luxembourg-latest.osm.bz2 /opt/osm_db osm-3s --meta'
+    runuser -l  osm_user -c 'wget -O planet.osm.bz2 "`cat /opt/osm_db/planet_url`"'
+    runuser -l  osm_user -c 'osm-3s/bin/init_osm3s.sh planet.osm.bz2 /opt/osm_db osm-3s --meta'
 fi
-
-
-
