28e198a7f3961fe1d909371b56622658886b47b4
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-06-12 06:12:02 +0200

    Populate data base from osm.pbf file

--- image/assets/boot.sh
+++ image/assets/boot.sh
@@ -8,7 +8,10 @@ chmod -R a+rw /opt/osm_db
 cd /home/osm_user
 if ! test -f /opt/osm_db/nodes.bin; then
     echo "No db found. Populating db..."
-    runuser -l  osm_user -c 'wget -O planet.osm.bz2 "`cat /opt/osm_db/planet_url`"'
-    runuser -l  osm_user -c 'osm-3s/bin/init_osm3s.sh planet.osm.bz2 /opt/osm_db osm-3s --meta'
-    runuser -l  osm_user -c 'cp -r /home/osm_user/osm-3s/rules /opt/osm_db/rules'
+    runuser -l osm_user -c 'wget -O planet.osm.pbf "`cat /opt/osm_db/planet_url`"'
+    runuser -l osm_user -c 'osmconvert planet.osm.pbf -o=planet.osm'
+    runuser -l osm_user -c 'bzip2 --fast planet.osm'
+    runuser -l osm_user -c 'osm-3s/bin/init_osm3s.sh planet.osm.bz2 /opt/osm_db osm-3s --meta'
+    runuser -l osm_user -c 'cp -r /home/osm_user/osm-3s/rules /opt/osm_db/rules'
+    rm planet.osm.pbf planet.osm.bz2
 fi
