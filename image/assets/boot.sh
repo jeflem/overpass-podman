c200a0abd21a773e27ca1b1a671e8a391c83b0a0
Jens Flemming <jens.flemming@fh-zwickau.de>
2024-06-13 07:49:31 +0200

    Avoid osm file

--- image/assets/boot.sh
+++ image/assets/boot.sh
@@ -9,8 +9,7 @@ cd /home/osm_user
 if ! test -f /opt/osm_db/nodes.bin; then
     echo "No db found. Populating db..."
     runuser -l osm_user -c 'wget -O planet.osm.pbf "`cat /opt/osm_db/planet_url`"'
-    runuser -l osm_user -c 'osmconvert planet.osm.pbf -o=planet.osm'
-    runuser -l osm_user -c 'cat planet.osm | /usr/local/bin/update_database --db-dir=/opt/osm_db --meta'
+    runuser -l osm_user -c 'osmconvert planet.osm.pbf | /usr/local/bin/update_database --db-dir=/opt/osm_db --meta'
     runuser -l osm_user -c 'cp -r /home/osm_user/osm-3s/rules /opt/osm_db/rules'
-    rm planet.osm.pbf planet.osm.bz2
+    rm planet.osm.pbf
 fi
